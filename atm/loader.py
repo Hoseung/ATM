2dbea38f2c08070d8381e54fc4444573d4850530
Hoseung <hopung@gmail.com>
2021-08-27 18:06:03 +0900

    Testing ResNet

--- atm/loader.py
+++ atm/loader.py
@@ -1,149 +1,83 @@
-# torchvision.datasets를 모방하면 됨. 
-
 from PIL import Image
-import os
-import os.path
-import numpy as np
-import pickle
 from typing import Any, Callable, Optional, Tuple
+import torchvision
+from torchvision.datasets.vision import VisionDataset
+from functools import partial
+import numpy as np
 
-from .vision import VisionDataset
-from .utils import check_integrity, download_and_extract_archive
+class TonemapImageDataset(VisionDataset):
+    def __init__(self, 
+                 data_array, 
+                 tmo,
+                 labels: Optional = None, 
+                 train: bool=True,
+                 transform: Optional[Callable] = None,
+                 target_transform: Optional[Callable] = None,):
+        self._array = data_array
+        self._good_gids = np.array([gal['img_name'] for gal in data_array])
+        self.img_labels = labels
+        self.transform = transform
+        self.target_transform = target_transform
+        self.tmo = tmo
+        self._bad_tmo=False
 
+    def _apply_tm(self, image):
+        try:
+            return self.tmo(image)
+        except ZeroDivisionError:
+            print("division by zero. Probably bad choice of TM parameters")
+            self._bad_tmo=True
+            return image
 
-class CIFAR10(VisionDataset):
-    """`CIFAR10 <https://www.cs.toronto.edu/~kriz/cifar.html>`_ Dataset.
-
-    Args:
-        root (string): Root directory of dataset where directory
-            ``cifar-10-batches-py`` exists or will be saved to if download is set to True.
-        train (bool, optional): If True, creates dataset from training set, otherwise
-            creates from test set.
-        transform (callable, optional): A function/transform that takes in an PIL image
-            and returns a transformed version. E.g, ``transforms.RandomCrop``
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
-        download (bool, optional): If true, downloads the dataset from the internet and
-            puts it in root directory. If dataset is already downloaded, it is not
-            downloaded again.
-
-    """
-    base_folder = 'cifar-10-batches-py'
-    url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
-    filename = "cifar-10-python.tar.gz"
-    tgz_md5 = 'c58f30108f718f92721af3b95e74349a'
-    train_list = [
-        ['data_batch_1', 'c99cafc152244af753f735de768cd75f'],
-        ['data_batch_2', 'd4bba439e000b95fd0a9bffe97cbabec'],
-        ['data_batch_3', '54ebc095f3ab1f0389bbae665268c751'],
-        ['data_batch_4', '634d18415352ddfa80567beed471001a'],
-        ['data_batch_5', '482c414d41f54cd18b22e5b47cb7c3cb'],
-    ]
-
-    test_list = [
-        ['test_batch', '40351d587109b95175f43aff81a1287e'],
-    ]
-    meta = {
-        'filename': 'batches.meta',
-        'key': 'label_names',
-        'md5': '5ff9c542aee3614f3951f8cda6e48888',
-    }
-
-    def __init__(
-            self,
-            root: str,
-            train: bool = True,
-            transform: Optional[Callable] = None,
-            target_transform: Optional[Callable] = None,
-            download: bool = False,
-    ) -> None:
-
-        super(CIFAR10, self).__init__(root, transform=transform,
-                                      target_transform=target_transform)
-
-        self.train = train  # training set or test set
-
-        if download:
-            self.download()
-
-        if not self._check_integrity():
-            raise RuntimeError('Dataset not found or corrupted.' +
-                               ' You can use download=True to download it')
-
-        if self.train:
-            downloaded_list = self.train_list
-        else:
-            downloaded_list = self.test_list
-
-        self.data: Any = []
-        self.targets = []
-
-        # now load the picked numpy arrays
-        for file_name, checksum in downloaded_list:
-            file_path = os.path.join(self.root, self.base_folder, file_name)
-            with open(file_path, 'rb') as f:
-                entry = pickle.load(f, encoding='latin1')
-                self.data.append(entry['data'])
-                if 'labels' in entry:
-                    self.targets.extend(entry['labels'])
-                else:
-                    self.targets.extend(entry['fine_labels'])
-
-        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
-        self.data = self.data.transpose((0, 2, 3, 1))  # convert to HWC
-
-        self._load_meta()
-
-    def _load_meta(self) -> None:
-        path = os.path.join(self.root, self.base_folder, self.meta['filename'])
-        if not check_integrity(path, self.meta['md5']):
-            raise RuntimeError('Dataset metadata file not found or corrupted.' +
-                               ' You can use download=True to download it')
-        with open(path, 'rb') as infile:
-            data = pickle.load(infile, encoding='latin1')
-            self.classes = data[self.meta['key']]
-        self.class_to_idx = {_class: i for i, _class in enumerate(self.classes)}
-
-    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+    def _to_8bit(self, image):
         """
-        Args:
-            index (int): Index
-
-        Returns:
-            tuple: (image, target) where target is index of the target class.
+        Normalize per image (or use global min max??)
         """
-        img, target = self.data[index], self.targets[index]
 
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
-        img = Image.fromarray(img)
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return img, target
-
-
+        image = (image - image.min())/image.ptp()
+        image *= 255
+        return image.astype('uint8')        
+    
     def __len__(self) -> int:
-        return len(self.data)
+        return len(self._array)
+    
+    def __getitem__(self, idx: int) -> Tuple[Any, Any]:
+        """
+        For super
+        """
+        image, _segmap, weight = self._array[idx]['data']
+        image[~_segmap.astype(bool)] = 0#np.nan # Is it OK to have nan?
+        image[image < 0] = 0
 
-    def _check_integrity(self) -> bool:
-        root = self.root
-        for fentry in (self.train_list + self.test_list):
-            filename, md5 = fentry[0], fentry[1]
-            fpath = os.path.join(root, self.base_folder, filename)
-            if not check_integrity(fpath, md5):
-                return False
-        return True
+        image = self._to_8bit(self._apply_tm(image))
+        image = Image.fromarray(image)
+        label = self.img_labels[idx]
+        
+        if self.transform is not None:
+            image = self.transform(image)
+            
+        return image, label
 
-    def download(self) -> None:
-        if self._check_integrity():
-            print('Files already downloaded and verified')
-            return
-        download_and_extract_archive(self.url, self.root, filename=self.filename, md5=self.tgz_md5)
 
-    def extra_repr(self) -> str:
-        return "Split: {}".format("Train" if self.train is True else "Test")
+    
+class TonemapImageDatasetPair(TonemapImageDataset):
+    """
+    returns two differently (randomly) transformed version of an image.
+    
+    img_labels = np.ndarray
+    """
+    def __getitem__(self, idx):
+        image, _segmap, weight = self._array[idx]['data']
+        image[~_segmap.astype(bool)] = 0#np.nan # Is it OK to have nan?
+        image[image < 0] = 0
+        
+        image = self._to_8bit(self._apply_tm(image))
+        image = Image.fromarray(image)
+        label = self.img_labels[idx]
+        
+        if self.transform is not None:
+            im_1 = self.transform(image) # random transform. 
+            im_2 = self.transform(image)
+
+        return (im_1, im_2), label
+        
